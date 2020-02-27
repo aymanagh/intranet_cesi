e0c165baa3c349cae32a1be05c7f8dc6b1e2410d
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-02-27 14:36:59 +0100

    Add Awesome Library + face.php

--- view/face.php
+++ view/face.php
@@ -7,13 +7,27 @@ include ('navbar.php')
         <meta charset="utf-8" />
         <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/all.min.css">
+
+        <script src="../assets/js/jquery.min.js"></script>
+
         <title>FAQ</title>
     </head>
-<body>
-    <div class="container-fluid" style="margin-top : 1em">
-        <div class="row">
-            <div class="col-sm-2 ">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+<body style="background-color : #f1f1f1;">
+    <div id="container-face" class="container" style="background-color :white; margin-top : 1em ;">
+        <div class="row text-center">
+            
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019 <br>
+                         Apprenant   </p>
+                        <a href="mailto:contact.cesi.fr"><i class="far fa-envelope"></i></a>
+                    </div>
+                </div>
+               
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -21,9 +35,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -31,9 +43,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -41,9 +51,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -51,9 +59,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -61,9 +67,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
@@ -71,17 +75,7 @@ include ('navbar.php')
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-2">
-                <div class="card border-dark mb-3" style="width: 12rem;">
-                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
-                    <div class="card-body text-dark">
-                        <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 </body>
