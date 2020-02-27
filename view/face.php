856bfd245b9fa184927cc91ee73e3763b5b26a92
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-02-27 17:17:33 +0100

    face.php modal + indentation

--- view/face.php
+++ view/face.php
@@ -1,81 +1,145 @@
-<?php
-include ('navbar.php')
-?>
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-        <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/all.min.css">
-
-        <script src="../assets/js/jquery.min.js"></script>
-
-        <title>FAQ</title>
+        <!-- Header -->
+        <?php include('header.php') ?>
+        <title>Trombinoscope</title>
     </head>
-<body style="background-color : #f1f1f1;">
-    <div id="container-face" class="container" style="background-color :white; margin-top : 1em ;">
-        <div class="row text-center">
-            
+    <body style="background-color : #f1f1f1;">
+    <!-- Navbar -->
+    <?php 
+        include('navbar.php')
+    ?>
+    <!-- Face Card -->
+        <div id="container-face" class="container">
+            <div class="row text-center">
+                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</br>Apprenant</p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="" data-toggle="modal" data-target="#exampleModalLong">
+                                    <span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="container-fluid">
+                                                    <div class="row border border-primary">
+                                                        <div class="col-md-4 border border-primary">PHOTO</div>
+                                                        <div class="col-md-4 ml-auto border border-primary">Promotion</div>
+                                                    </div>
+                                                    <div class="row border border-primary">
+                                                        <div class="col-md-12 ml-auto">.col-md-12</div>
+                                                    </div>                                          
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    <!-- Face Card TEST -->
+
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
                         <p class="card-text">Promo : RIL-2019 <br>
-                         Apprenant   </p>
-                        <a href="mailto:contact.cesi.fr"><i class="far fa-envelope"></i></a>
-                    </div>
-                </div>
-               
-                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
-                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
-                    <div class="card-body text-dark">
-                        <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
-                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
-                    <div class="card-body text-dark">
-                        <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        Apprenant   </p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#"><span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Promo : RIL-2019 <br>
+                        Apprenant   </p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#"><span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Promo : RIL-2019 <br>
+                        Apprenant   </p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#"><span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Promo : RIL-2019 <br>
+                        Apprenant   </p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#"><span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
                     <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
                     <div class="card-body text-dark">
                         <h5 class="card-title">Chuck Norris</h5>
-                        <p class="card-text">Promo : RIL-2019</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Promo : RIL-2019 <br>
+                        Apprenant   </p>
+                        <div class="row border-top border-dark">
+                            <div class="col-sm-6">
+                                <a href="mailto:contact.cesi.fr"><span title="Contacter Chuck Norris"><i class="far fa-envelope"></i></span></a>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#"><span title="Profil détaillé"><i class="far fa-address-card"></i></span></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+</html>
--- view/navbar.php
+++ view/navbar.php
@@ -1,11 +1,3 @@
-<php></php>
-
-<link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
-<script src="../bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js"></script>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Intranet - CESI</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,16 +21,6 @@
       <li class="nav-item">
         <a class="nav-link" href="faq.php">FAQ</a>
       </li>
-      <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Dropdown link
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Link 1</a>
-        <a class="dropdown-item" href="#">Link 2</a>
-        <a class="dropdown-item" href="#">Link 3</a>
-      </div>
-    </li>
     </ul>
   </div>
 </nav>
