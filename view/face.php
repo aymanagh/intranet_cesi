ead6a210981324f9257819e85780413d881912a4
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-02-28 00:02:11 +0100

    modal face.php

--- view/face.php
+++ view/face.php
@@ -14,8 +14,8 @@
         <div id="container-face" class="container">
             <div class="row text-center">
                 <div class="text-center card border-dark mb-3 mt-1 ml-1 mr-1" style="width: 12rem;">
-                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
-                    <div class="card-body text-dark">
+                    <img id ="ok"class=" rounded-circle  card-img-top " src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body ">
                         <h5 class="card-title">Chuck Norris</h5>
                         <p class="card-text">Promo : RIL-2019</br>Apprenant</p>
                         <div class="row border-top border-dark">
@@ -29,26 +29,33 @@
                                         <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                            <div class="modal-header" style ="background: #ffc853" >
+                                                <h5 class="modal-title color-cesi-black" id="exampleModalLongTitle">Fiche détaillée (Prénom Nom)</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
                                             <div class="modal-body">
                                                 <div class="container-fluid">
-                                                    <div class="row border border-primary">
-                                                        <div class="col-md-4 border border-primary">PHOTO</div>
-                                                        <div class="col-md-4 ml-auto border border-primary">Promotion</div>
+                                                    <div class="row ">
+                                                        <div id="imageContainer" class="  col-md-4 ">
+                                                            <img src="../assets/cesi.jpg" id="test" alt="photo" style="width: 100%;">
+                                                        </div>
+                                                        <div class="col-md-1 ml-auto"style="background: #ffc853" ></div>
+                                                        <div class=" test col-md-7 ml-auto text-left">
+                                                            <span class="blockquote-footer">Promotion : RIL 2019</span></br>
+                                                            <span class="blockquote-footer">Campus : CESI Le Mans </span></br>
+                                                            <span class="blockquote-footer">Entreprise : </span></br>
+                                                        </div>
                                                     </div>
-                                                    <div class="row border border-primary">
-                                                        <div class="col-md-12 ml-auto">.col-md-12</div>
+                                                    </br>
+                                                    <div class="row ">
+                                                        <div class="col-md-12 ml-auto h4" ><a href="mailto:contact.cesi.fr">Edward.snowden@viacesi.fr</a></div>
                                                     </div>                                          
                                                 </div>
                                             </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            <div class="modal-footer " style="background: #ffc853">
+                                                <button type="button" class="btn my-btn" data-dismiss="modal">Close</button>
                                             </div>
                                             </div>
                                         </div>
@@ -140,6 +147,5 @@
                     </div>
                 </div>
             </div>
-        </div>
     </body>
 </html>
