f516e728de9a4f882357329a5e4ecf015b3bd7c6
MarjorieMeunier <61453391+MarjorieMeunier@users.noreply.github.com>
2020-02-27 14:37:51 +0100

    navbar

--- view/header.php
+++ view/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
-<header>
+<head>
     <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 
+    
     <script src="../assets/js/jquery.min.js"></script>
+    <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
     <script src="../assets/js/script.js"></script>
-</header>
+</head>
 
--- view/navbar.php
+++ view/navbar.php
@@ -1,30 +1,40 @@
-<!DOCTYPE html>
-<html>
+      <!DOCTYPE html>
+      <html>
 
+      <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Intranet - CESI</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Intranet - CESI</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Accueil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Promo</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="event.php">Evènements</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Messagerie instantanée</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="faq.php">FAQ</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Promo</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="event.php">Evènements</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Messagerie instantanée</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="faq.php">FAQ</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  NOM PRENOM UTILISATEUR
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Déconnexion</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
