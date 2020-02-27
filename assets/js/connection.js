23d74cbec1cdac57e92349b0d463c744e6942583
aymanagh <47530849+aymanagh@users.noreply.github.com>
2020-02-27 10:46:35 +0100

    'git'

--- assets/js/connection.js
+++ assets/js/connection.js
@@ -3,22 +3,21 @@ $(document).ready(function() {
         var mail = $('#inputEmail').val();
         var mdp = $('#inputPassword').val();
 
-        alert(mail + " " + mdp);
-        // $.ajax({
+        //alert(mail + " " + mdp);
+        $.ajax({
 
-        //     url: 'controllers/controllers.php',
-        //     type: 'POST',
-        //     data: {
-        //         mail: mail,
-        //         mdp: mdp
-        //     },
-        //     dataType: 'json',
-        //     success: function(data) {
-        //         alert('Data: ' + data);
-        //     },
-        //     error: function(request, error) {
-        //         alert("Request: " + JSON.stringify(request));
-        //     }
-        // });
+            url: '../../controllers/controllers.php',
+            type: 'POST',
+            data: {
+                mail: mail,
+                mdp: mdp
+            },
+            success: function(data) {
+                alert('Data: ' + data);
+            },
+            error: function(request, error) {
+                alert("Request: " + request);
+            }
+        });
     });
 });
--- controllers/controllers.php
+++ controllers/controllers.php
@@ -1,5 +1,8 @@
 <?php
 
+if(isset['mail'] && isset['mdp']){
+    return 'ok';
+}
 
 
 
