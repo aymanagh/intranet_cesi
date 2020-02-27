a1daad598f770fabec3ad0135604013c44cfe510
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-02-27 11:31:19 +0100

    face.php

--- view/face.php
+++ view/face.php
@@ -6,365 +6,82 @@ include ('navbar.php')
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
         <title>FAQ</title>
     </head>
 <body>
-<main>
-		</br>
-		<div class="row">
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/dorian.jpeg">
-						<span class="card-title">Dorian GÉMY</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Dorian GÉMY<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:dorian.gemy@viacesi.fr" target="BLANK"
-									class="lienMail">dorian.gemy@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								<a href="https://aznetwork.eu" target="BLANK">AZNetwork</a>
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/justin.jpeg">
-						<span class="card-title">Justin BAHIER</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Justin BAHIER<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:justin.bahier@viacesi.fr" target="BLANK"
-									class="lienMail">justin.bahier@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								<a href="http://buisard-distribution.fr" target="BLANK">Buisard</a>
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/thibaut.jpeg">
-						<span class="card-title">Thibaut DESFOURS</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Thibaut DESFOURS<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:thibaut.desfours@viacesi.fr" target="BLANK"
-									class="lienMail">thibaut.desfours@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								<a href="https://aznetwork.eu" target="BLANK">AZNetwork</a>
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/michael.jpeg">
-						<span class="card-title">Michaël NGUYEN</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Brancheur de cable</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Michaël NGUYEN<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center>
-								<i class="material-icons">email</i></br>
-								<a href="mailto:michael.nguyen@viacesi.fr" target="BLANK"
-									class="lienMail">michael.nguyen@viacesi.fr</a></br>
-								<i class="material-icons">straighten</i></br>
-								1.20 m</br>
-								<i class="material-icons">business_center</i></br>
-								<a href="https://aznetwork.eu" target="BLANK">AZNetwork</a>
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/ayman.jpeg">
-						<span class="card-title">Ayman AGHARBI</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Ayman AGHARBI<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:ayman.agharbi@viacesi.fr" target="BLANK"
-									class="lienMail">ayman.agharbi@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Renault SAS
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/arthur.jpeg">
-						<span class="card-title">Arthur CHERAMY</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Arthur CHERAMY<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:arthur.cheramy@viacesi.fr" target="BLANK"
-									class="lienMail">arthur.cheramy@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Mécachrome France
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/antoineC.jpeg">
-						<span class="card-title">Antoine CORGNIARD</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Antoine CORGNIARD<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:antoine.corgniard@viacesi.fr" target="BLANK"
-									class="lienMail">antoine.corgniard@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								A2Display
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/antoineH.jpeg">
-						<span class="card-title">Antoine HOULBERT</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Antoine HOULBERT<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:antoine.houlbert@viacesi.fr" target="BLANK"
-									class="lienMail">antoine.houlbert@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Studeffi
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/enzo.jpeg">
-						<span class="card-title">Enzo JURET</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Enzo JURET<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:enzo.juret@viacesi.fr" target="BLANK"
-									class="lienMail">enzo.juret@viacesi.fr</a></br>
-								<i class="material-icons">business_center</i></br>
-								Conseil Dep. Sarthe
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/kevin.jpeg">
-						<span class="card-title">Kevin LEHOUX</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Kevin LEHOUX<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:kevin.lehoux@viacesi.fr" target="BLANK"
-									class="lienMail">kevin.lehoux@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Renault SAS
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/guillaume.jpeg">
-						<span class="card-title">Guillaume M BALI</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Guillaume M BALI<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:guillaume.mbali@viacesi.fr" target="BLANK"
-									class="lienMail">guillaume.mbali@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Mairie d'Avrille
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/mansour.jpeg">
-						<span class="card-title">Mansour OZDAMIROV</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Mansour OZDAMIROV<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:mansour.ozdamirov@viacesi.fr" target="BLANK"
-									class="lienMail">mansour.ozdamirov@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Renault SAS
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/alexis.jpeg">
-						<span class="card-title">Alexis POUPELIN</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Alexis POUPELIN<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:alexis.poupelin@viacesi.fr" target="BLANK"
-									class="lienMail">alexis.poupelin@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Fleury Michon SA
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col s6 m2">
-				<div class="card">
-					<div class="card-image waves-effect waves-block waves-light">
-						<img class="activator" src="../images/marvyn.jpeg">
-						<span class="card-title">Marvyn ROCHER</span>
-					</div>
-					<div class="card-content">
-						<p>
-							<center>Apprenant</center>
-						</p>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title grey-text text-darken-4">Marvyn ROCHER<i
-								class="material-icons right">close</i></span>
-						<p>
-							<center><i class="material-icons">email</i></br>
-								<a href="mailto:marvyn.rocher@viacesi.fr" target="BLANK"
-									class="lienMail">marvyn.rocher@viacesi.fr</a>
-								<i class="material-icons">business_center</i></br>
-								Linkintouch
-							</center>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
+    <div class="container-fluid" style="margin-top : 1em">
+        <div class="row">
+            <div class="col-sm-2 ">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="card border-dark mb-3" style="width: 12rem;">
+                    <img class="card-img-top" src="../assets/cesi.jpg" alt="Card image cap">
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">Chuck Norris</h5>
+                        <p class="card-text">Promo : RIL-2019</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
