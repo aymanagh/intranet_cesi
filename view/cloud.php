3eb058732c1a03a1b717df9a7b6122d5358dd824
stef-gitHub <41380342+stef-gitHub@users.noreply.github.com>
2020-03-08 23:58:09 +0100

    cloud.php

--- view/cloud.php
+++ view/cloud.php
@@ -10,10 +10,49 @@
         <?php include('navbar.php')?>
         <div class="container container-cesi" style="margin-bottom : 1em" >
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-12 text-center">
                     <h1>Téléchargez les cours numériques ici</h1>
+                    </br>
                 </div>
             </div>
+        
+            <h2>Cours des RIL-2019</h2>
+                    
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Matière</th>
+                    <th>Nom du cours</th>
+                    <th>Intervenant</th>
+                    <th>Télécharger</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>12/02/85</td>
+                    <td>MySQL</td>
+                    <td>Base de données</td>
+                    <td>Florian Pichon</td>
+                    <td>Fichier</td>
+                </tr>
+                <tr>
+                    <td>12/02/85</td>
+                    <td>MySQL</td>
+                    <td>Base de données</td>
+                    <td>Florian Pichon</td>
+                    <td>Fichier</td>
+                </tr>
+                <tr>
+                    <td>12/02/85</td>
+                    <td>MySQL</td>
+                    <td>Base de données</td>
+                    <td>Florian Pichon</td>
+                    <td>Fichier</td>
+                </tr>
+                </tbody>
+            </table>
+            </br>
         </div>
         
 
